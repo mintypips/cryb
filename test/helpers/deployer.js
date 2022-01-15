44299216aaa0f8d37501d300cd6a95a8dfe4b199
ppoliani <ppoliani@gmail.com>
2022-01-15 09:42:45 +0000

    test(*): add multiple helper functions

--- test/helpers/deployer.js
+++ test/helpers/deployer.js
@@ -1,5 +1,7 @@
-const {Contract} = require('../helpers/utils')
+const {upgrades} = require('hardhat')
 const {getTreasury} = require('./accounts')
+const {Contract} = require('./utils')
+const {duration} = require('./time')
 
 const deployCrybToken = async () => {
   const treasury = await getTreasury()
@@ -10,6 +12,30 @@ const deployCrybToken = async () => {
   )
 }
 
+const deployCrybCrowdsale = async (
+  tax=500, //5%
+  rate=10, // price per token 0.1 ETH
+  startTs,
+  endTs,
+  vestingDuration=duration.days(10),
+  cliff=0
+) => {
+  const treasury = await getTreasury()
+  const CrybToken = await Contract('CrybToken')
+
+  const crybToken =  await CrybToken.deploy(
+    tax,
+    treasury.address
+  )
+
+  const crybToken = await upgrades.deployProxy(CrybToken, [
+    crybToken.address,
+    treasury.address,
+
+  ])
+}
+
 module.exports = {
-  deployCrybToken
+  deployCrybToken,
+  deployCrybCrowdsale
 }
