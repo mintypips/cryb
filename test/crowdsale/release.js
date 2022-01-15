594c9fd8957dca4b0922a9b5e5d8b158e356c00a
ppoliani <ppoliani@gmail.com>
2022-01-15 12:58:45 +0000

    test(*): multiple users release

--- test/crowdsale/release.js
+++ test/crowdsale/release.js
@@ -122,6 +122,91 @@ describe('CrybCrowdsale: release', () => {
     expect(await crybToken.balanceOf(participants[0].address)).to.equal(toBase(300 + 500))
   })
 
+  it('should support release vested position for multiple users', async () => {
+    await moveToStartTime()
+    // total amount ready to be vested is 300
+    await crybCrowdsale.connect(participants[0]).buy({value: toBase(30)})
+    const vestingInfo = await crybCrowdsale.getVestingInfo(participants[0].address, 0)
+
+    // the first 4 days users releases linearly the vested tokens from position 1
+    for (let i = 1; i <= 4; i++) {
+      // move one day from the vesting start time
+      await setNextBlockTimestamp(toSolTime(await addDays(i, new Date(fromSolTime(vestingInfo.startTime)))))
+
+      // release
+      let balanceBefore = await crybToken.balanceOf(participants[0].address)
+      await crybCrowdsale.connect(participants[0]).release(0)
+      let balanceAfter = await crybToken.balanceOf(participants[0].address)
+      
+      // should get 1/10 of the total 300 tokens vested
+      expect(balanceAfter.sub(balanceBefore)).to.equal(toBase(30))
+    }
+
+    // 5 days later the another user buys tokens
+    await setNextBlockTimestamp(toSolTime(await addDays(5, new Date(fromSolTime(vestingInfo.startTime)))))
+    await crybCrowdsale.connect(participants[1]).buy({value: toBase(50)})
+
+    // from day 6 and on user will be able to release from both vesting positions
+    for (let i=6; i <= 10; i++) {
+      await setNextBlockTimestamp(toSolTime(await addDays(i, new Date(fromSolTime(vestingInfo.startTime)))))
+
+      // release user 1
+      let balanceBefore = await crybToken.balanceOf(participants[0].address)
+      await crybCrowdsale.connect(participants[0]).releaseAll()
+      let balanceAfter = await crybToken.balanceOf(participants[0].address)
+
+      if(i === 6) {
+        // should get 2/10 of the total 300 tokens because position 0 was last released on day 4
+        expect(balanceAfter.sub(balanceBefore)).to.equal(toBase(60))
+      }
+      else {
+        expect(balanceAfter.sub(balanceBefore)).to.equal(toBase(30))
+      }
+
+      // release user 2
+      balanceBefore = await crybToken.balanceOf(participants[1].address)
+      await crybCrowdsale.connect(participants[1]).releaseAll()
+      balanceAfter = await crybToken.balanceOf(participants[1].address)
+      
+      if(i === 6) {
+        // slightly more that 50 in the first iteration
+        expect(balanceAfter.sub(balanceBefore)).to.equal('50000578703703703703')
+      }
+      else {
+        expect(balanceAfter.sub(balanceBefore)).to.equal(toBase(50))
+      }
+    }
+
+    expect(await crybToken.balanceOf(participants[0].address)).to.equal(toBase(300))
+    expect(await crybToken.balanceOf(participants[1].address)).to.equal('250000578703703703703')
+
+    // from day 11 until day 15 only participant 1 can release; participant 0 has released everything
+    for (let i=11; i <= 15; i++) {
+      await setNextBlockTimestamp(toSolTime(await addDays(i, new Date(fromSolTime(vestingInfo.startTime)))))
+
+      let balanceBefore = await crybToken.balanceOf(participants[0].address)
+      await crybCrowdsale.connect(participants[0]).releaseAll()
+      let balanceAfter = await crybToken.balanceOf(participants[0].address)
+      expect(balanceAfter.sub(balanceBefore)).to.equal(0)
+
+      balanceBefore = await crybToken.balanceOf(participants[1].address)
+      await crybCrowdsale.connect(participants[1]).releaseAll()
+      balanceAfter = await crybToken.balanceOf(participants[1].address)
+
+      if(i === 15) {
+        // for the same reason as above
+        expect(balanceAfter.sub(balanceBefore)).to.equal('49999421296296296297')
+      }
+      else {
+        expect(balanceAfter.sub(balanceBefore)).to.equal(toBase(50))
+      }
+    }
+
+    // on day 15 all position 2 is fully vested and position 1 remains intact as it as fully released on day 10
+    expect(await crybToken.balanceOf(participants[0].address)).to.equal(toBase(300))
+    expect(await crybToken.balanceOf(participants[1].address)).to.equal(toBase(500))
+  })
+
   it('should emit Claimed', async () => {
     await moveToStartTime()
     // total amount ready to be vested is 300
